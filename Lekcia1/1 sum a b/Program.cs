fc17f0b29e9ae7a608d4212b8f2987421807da08
Anvar <anv@list.com>
2022-11-26 22:24:10 +0400

    Ura Masha

---
+++ Lekcia1/1 sum a b/Program.cs
@@ -0,0 +1,8 @@
+﻿// пример 1 лекции 1 предпод Каменецкий
+
+int namberA = new Random().Next(1, 10);
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
+int result = namberA + namberB;
+Console.WriteLine(result);
---
+++ Lekcia1/3 Giri/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача по нахождению самой тяжелой гири
+int a = 2;
+int b = 3;
+int c = 8;
+int d = 6;
+int e = 4;
+int max = a;
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+
+Console.Write("Самая тяжелая гиря - ");
+Console.WriteLine(max+" кг");
